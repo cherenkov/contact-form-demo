4d74d781cc66b003d1da78f2bc091d47cd348474
cherenkov <chrnkv@gmail.com>
2021-12-13 01:21:01 +0900

    最小構成のバリデーション追加

--- src/components/screens/ContactForm.tsx
+++ src/components/screens/ContactForm.tsx
@@ -1,8 +1,22 @@
-import { useAuthState } from '~/components/contexts/UserContext'
-import { Head } from '~/components/shared/Head'
+import { useAuthState } from '~/components/contexts/UserContext';
+import { Head } from '~/components/shared/Head';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface IFormInput {
+  name: String;
+  email: String;
+}
 
 function Index() {
-  const { state } = useAuthState()
+  const { state } = useAuthState();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -10,20 +24,25 @@ function Index() {
       <div className="container mx-auto px-8 mt-8">
         <div className="max-w-md mx-auto">
           <h1 className="text-2xl font-bold mb-6">お問い合わせフォーム</h1>
-          <div className="grid grid-cols-1 gap-6">
-            <label className="block">
-              <span className="text-gray-700">Full name</span>
-              <input
-                type="text"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder=""
-              />
-            </label>
-            <label className="block">
-              <span className="text-gray-700">Email address</span>
-              <input
-                type="email"
-                className="
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid grid-cols-1 gap-6">
+              <label className="block">
+                <span className="text-gray-700">名前(姓名)</span>
+                <input
+                  type="text"
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder=""
+                  {...register('name', { required: '名前は必須です。' })}
+                />
+              </label>
+              {errors.name && (
+                <span className="text-sm font-bold tracking-wide text-red-500">{errors.name?.message}</span>
+              )}
+              <label className="block">
+                <span className="text-gray-700">メールアドレス</span>
+                <input
+                  type="email"
+                  className="
                   mt-1
                   block
                   w-full
@@ -32,13 +51,23 @@ function Index() {
                   shadow-sm
                   focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                 "
-                placeholder="john@example.com"
-              />
-            </label>
-            <label className="block">
-              <span className="text-gray-700">What type of event is it?</span>
-              <select
-                className="
+                  placeholder="john@example.com"
+                  {...register('email', {
+                    required: 'メールアドレスは必須です。',
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: '正しいメールアドレスを入力してください。',
+                    },
+                  })}
+                />
+              </label>
+              {errors.email && (
+                <span className="text-sm font-bold tracking-wide text-red-500">{errors.email?.message}</span>
+              )}
+              <label className="block">
+                <span className="text-gray-700">What type of event is it?</span>
+                <select
+                  className="
                   block
                   w-full
                   mt-1
@@ -47,17 +76,17 @@ function Index() {
                   shadow-sm
                   focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                 "
-              >
-                <option>Corporate event</option>
-                <option>Wedding</option>
-                <option>Birthday</option>
-                <option>Other</option>
-              </select>
-            </label>
-            <label className="block">
-              <span className="text-gray-700">Additional details</span>
-              <textarea
-                className="
+                >
+                  <option>Corporate event</option>
+                  <option>Wedding</option>
+                  <option>Birthday</option>
+                  <option>Other</option>
+                </select>
+              </label>
+              <label className="block">
+                <span className="text-gray-700">Additional details</span>
+                <textarea
+                  className="
                   mt-1
                   block
                   w-full
@@ -66,16 +95,16 @@ function Index() {
                   shadow-sm
                   focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                 "
-                rows="3"
-              ></textarea>
-            </label>
-            <div className="block">
-              <div className="mt-2">
-                <div>
-                  <label className="inline-flex items-center">
-                    <input
-                      type="checkbox"
-                      className="
+                  rows="3"
+                />
+              </label>
+              {/* <div className="block">
+                <div className="mt-2">
+                  <div>
+                    <label className="inline-flex items-center">
+                      <input
+                        type="checkbox"
+                        className="
                         rounded
                         border-gray-300
                         text-indigo-600
@@ -86,18 +115,24 @@ function Index() {
                         focus:ring-indigo-200
                         focus:ring-opacity-50
                       "
-                      checked
-                    />
-                    <span className="ml-2">Email me news and special offers</span>
-                  </label>
+                        checked
+                      />
+                      <span className="ml-2">Email me news and special offers</span>
+                    </label>
+                  </div>
                 </div>
+              </div> */}
+              <div className="mt-4">
+                <button type="submit" className="btn btn-outline btn-primary">
+                  確認画面へ
+                </button>
               </div>
             </div>
-          </div>
+          </form>
         </div>
-        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
