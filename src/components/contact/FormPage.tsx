6317e2c70bf8e1d67f9a2465f929823dc091a415
cherenkov <chrnkv@gmail.com>
2021-12-14 01:51:18 +0900

    どの製品についての選択肢にブランクなものを追加

--- src/components/contact/FormPage.tsx
+++ src/components/contact/FormPage.tsx
@@ -153,6 +153,7 @@ const FormPage = ({ handleNext }: Props) => {
               className="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               {...register('whichProduct', {})}
             >
+              <option>---</option>
               <option>Aサービスについて</option>
               <option>Bサービスについて</option>
               <option>Cサービスについて</option>
